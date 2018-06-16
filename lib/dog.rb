1116adc56fbd90bdd16d12fe3dc62b98cd777d02
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-06-16 01:35:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,13 +4,12 @@ def initialize(name,breed)
   @name=new_name
   @breed=new_breed
 end
-
+def name=(new_name)
+@name=new_name
 def name
   @name
 end
-def name=(new_name)
-@name=new_name
-end
+
 def breed=(new_breed)
   @breed=new_breed
 end
